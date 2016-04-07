501558e25b39ac39f77838d5598d84686cc88df7
Yawei Wang <yaweiw@microsoft.com>
2016-04-07 17:33:07 +0800

    cc

---
+++ Mapper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.SetIn(new StreamReader(args[0]));
+            }
+
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Reducer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            var count = 0;
+
+            if (args.Length > 0)
+            {
+                Console.SetIn(new StreamReader(args[0]));
+            }
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                count += line.Count(cr => (cr == ' ' || cr == '\n'));
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
