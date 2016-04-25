f10a53abe72095fb0179feb60af6ddb4ed1ae5ad
Yawei Wang <yaweiw@microsoft.com>
2016-04-25 17:37:29 +0800

    1

--- HtmlGenerator/Program.cs
+++ HtmlGenerator/Program.cs
@@ -1,9 +1,9 @@
 ﻿using HtmlGenerator;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager 
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+using Microsoft.WindowsAzure.Storage.Auth;
 
 namespace HtmlGeneratorCS
 {
@@ -11,18 +11,34 @@ namespace HtmlGeneratorCS
     {
         static void Main(string[] args)
         {
+            string constr = @"DefaultEndpointsProtocol=https;AccountName=yaweiw;AccountKey=account-key";
+            // Retrieve storage account from connection string.
+            StorageCrentials cre = new StorageCrentials();
+            CloudStorageAccount storageAccount = new CloudStorageAccount();
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference("photos");
+
             ReportGenerator rg = new ReportGenerator();
-            using (System.IO.StreamWriter output = new System.IO.StreamWriter(@"C:\Work\output.html"))
+            using (Stream memorystream = new MemoryStream())
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(memorystream))
             {
-                output.WriteLine(rg.HtmlHeaderBeginTag.ToString());
-                output.WriteLine(rg.HtmlBodyBeginTag.ToString());
-                output.WriteLine(rg.HtmlReportHeader.ToString());
-                output.WriteLine(rg.HtmlBuildSummary.ToString());
-                output.WriteLine(rg.HtmlBuildFiles.ToString());
-                output.WriteLine(rg.HtmlBuildDetails.ToString());
-                output.WriteLine(rg.HtmlBodyEndTag.ToString());
-                output.WriteLine(rg.HtmlRawLog.ToString());
-                output.WriteLine(rg.HtmlHeaderEndTag.ToString());
+                writer.WriteLine(rg.HtmlHeaderBeginTag.ToString());
+                writer.WriteLine(rg.HtmlBodyBeginTag.ToString());
+                writer.WriteLine(rg.HtmlReportHeader.ToString());
+                writer.WriteLine(rg.HtmlBuildSummary.ToString());
+                writer.WriteLine(rg.HtmlBuildFiles.ToString());
+                writer.WriteLine(rg.HtmlBuildDetails.ToString());
+                writer.WriteLine(rg.HtmlBodyEndTag.ToString());
+                writer.WriteLine(rg.HtmlRawLog.ToString());
+                writer.WriteLine(rg.HtmlHeaderEndTag.ToString());
+                var blob = GetBlockBlobReference(path);
+                blob.Properties.ContentType = contentType ?? "application/octet-stream";
+                await blob.UploadFromStreamAsync(source);
+
             }
         }
     }
