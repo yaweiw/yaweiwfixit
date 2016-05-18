37105cb9458b55c40e6fd4d9de7e39ec1cd3a1f0
Yawei Wang <yaweiw@microsoft.com>
2016-05-18 17:33:09 +0800

    dynamichandlegenerators

--- DTDynMethod/Program.cs
+++ DTDynMethod/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using DynamicMethodHandlerFactoryNameSpace;
 using System.Reflection;
+using System.Diagnostics;
 
 namespace DTDynMethod
 {
     public class People
     {
+        public string sth;
         public string Name { get; set; }
         public uint Age { get; set; }
         public People() : this(string.Empty, 0)
@@ -13,6 +14,7 @@ namespace DTDynMethod
         }
         public People(string name, uint age)
         {
+            sth = name;
             Name = name;
             Age = age;
         }
@@ -20,6 +22,15 @@ namespace DTDynMethod
         {
             return "Name is: " + Name + ". Age is: " + Age.ToString() + ".";
         }
+        public string GetDetails(int age)
+        {
+            return "s";
+        }
+        public string GetDetails(char c)
+        {
+            return "s";
+        }
+
     }
     class Program
     {
@@ -32,18 +43,50 @@ namespace DTDynMethod
         Uri URI => new Uri(@"http://abab/");
         static void Main(string[] args)
         {
-            Program p = new Program();
-            var x = typeof(int?);
-            var y = typeof(int);
-            Console.WriteLine(p.Name);
-            Console.WriteLine(p.URI.ToString());
+            string sth_local = "";
 
-            ConstructorInfo cinfo = typeof(People).GetConstructor(ctorFlags, null, Type.EmptyTypes, null);
-            var ctor = DynamicMethodHandlerFactory<People, People, People>.CreateConstructor(cinfo);
+            People p = new People("a", 10);
+            string s = p.GetDetails('c');
+            string ss = p.GetDetails(12);
+            Stopwatch stopWatch1 = new Stopwatch();
+            stopWatch1.Start();
+            for (int i = 999999999; i >= 0; i--)
+            {
+                p.Name = "a";
+                p.Name = "b";
+            }
+            stopWatch1.Stop();
+            TimeSpan ts1 = stopWatch1.Elapsed;
+            string elapsedTime1 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+    ts1.Hours, ts1.Minutes, ts1.Seconds,
+    ts1.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime1);
 
-            People ppp = ctor(typeof(People));
+            Stopwatch stopWatch2 = new Stopwatch();
+            stopWatch2.Start();
+            for (int i = 999999999; i >= 0; i--)
+            {
+                p.sth = "a";
+                p.sth = "b";
+            }
+            stopWatch2.Stop();
+            TimeSpan ts2 = stopWatch2.Elapsed;
+            string elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+    ts2.Hours, ts2.Minutes, ts2.Seconds,
+    ts2.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime2);
 
-            ctor(ppp);
+            Stopwatch stopWatch3 = new Stopwatch();
+            stopWatch3.Start();
+            sth_local = "a";
+            sth_local = "b";
+            stopWatch3.Stop();
+            TimeSpan ts3 = stopWatch3.Elapsed;
+            string elapsedTime3 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+    ts3.Hours, ts3.Minutes, ts3.Seconds,
+    ts3.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime3);
+
 
             Console.ReadLine();
 
--- Timer/Program.cs
+++ Timer/Program.cs
@@ -6,8 +6,26 @@
 
     public class Timer1
     {
+        public static string GetDetails(string s)
+        {
+            return "s";
+        }
+        public static string GetDetails(char c)
+        {
+            return "c";
+        }
+        public static string GetDetails(char c, string s)
+        {
+            return "cs";
+        }
+        public static string GetDetails(string s, char c)
+        {
+            return "sc";
+        }
         public static void Main()
         {
+            string sc = GetDetails("abab", 'c');
+            string cs = GetDetails('c' , "abab");
             Task mytask = new Task(() => Console.WriteLine("hi"));
             System.Timers.Timer aTimer = new System.Timers.Timer();
             aTimer.Elapsed += new ElapsedEventHandler((object source, ElapsedEventArgs e)=> Console.WriteLine("hi"));
